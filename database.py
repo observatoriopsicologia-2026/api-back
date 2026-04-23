a38d1fc256cfdab1105050957d9b3e8495858fc3
Sala 3 <sala3@FI-LabSistemas.ucatolica.loc>
2026-04-23 10:38:12 -0500

    Fix conexión MySQL, scripts de prueba y dependencias

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-DATABASE_URL = "mysql+pymysql://root:root@127.0.0.1:3306/sistema_academico"
+DATABASE_URL = "mysql+pymysql://u831733382_observatorio:JJj-1000256252*@srv1534.hstgr.io:3306/u831733382_observatorio"
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from database import Base, engine
 
-# Importar routers corregidos
+
 from routers import events, news, publications, researchers
 
-# Crear las tablas
-Base.metadata.create_all(bind=engine)
+
+
+print("Tablas creadas")
 
 app = FastAPI(title="Sistema Académico")
 
-# Registrar routers
 app.include_router(events.router)
 app.include_router(news.router)
 app.include_router(publications.router)

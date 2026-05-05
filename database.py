3d689e6cb332b7da0a8c15e722a909e1a3caf696
Tatianalopez18 <stlopez43@ucatolica.edu.co>
2026-05-05 14:47:28 -0500

    modificación database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "mysql+pymysql://u831733382_observatorio:JJj-1000256252*@srv1534.hstgr.io:3306/u831733382_observatorio"
-
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
